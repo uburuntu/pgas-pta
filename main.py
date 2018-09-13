c2d274b72bb52d643c6eaf326300e251929a511b
uburuntu <bekbulatov.ramzan@ya.ru>
2018-09-14 01:30:23 +0300

    new: fill sheet

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import passwords
 from gspread_handle import GSpread
-from lmsu_handle import LomonosovMSU
 from utils import range_grid
 
 if __name__ == '__main__':
     # MSU
-    lmsu = LomonosovMSU(username=passwords.auth_login, password=passwords.auth_pswd)
-    data = lmsu.scrap_data('users.csv')
-    data = lmsu.scrap_achievements(data)
+    # lmsu = LomonosovMSU(username=passwords.auth_login, password=passwords.auth_pswd)
+    # data = lmsu.scrap_data('users.csv')
+    # data = lmsu.scrap_achievements(data)
 
     # Google table
     worksheet = GSpread('key.json').get_worksheet()
 
-    # worksheet.acell('A1').value  # посомтреть что в ячейке
-    # print(worksheet.cell(1, 2).value)  # тоже самое только по номеру (строка, столбец)
-    # worksheet.add_rows(1)  # добавить строку
-    # worksheet.update_acell('B1', 'Bingo!')
-    # worksheet.update_cell(1, 2, 'Bingo!')
+    # Fill sheet
+    cells = worksheet.range(range_grid((1, 1), (1, 5)))
+    cells[0].value = 'ID'
+    cells[1].value = 'ФИО'
+    cells[2].value = 'Номер группы'
+    cells[3].value = 'Баллы'
+    cells[4].value = 'URL'
+    worksheet.update_cells(cells)
 
-    # Change cells
-    cell_list = worksheet.range(range_grid((1, 1), (3, 3)))
-    print(cell_list)
-    for cell in cell_list:
-        cell.value = 'kek'
-
-    # Update cells
-    worksheet.update_cells(cell_list)
+    for i, (user_id, user) in enumerate(data.items(), start=2):
+        cells = worksheet.range(range_grid((i, 1), (i, 5)))
+        cells[0].value = user_id
+        cells[1].value = user['name']
+        cells[2].value = 'хз'
+        cells[3].value = sum([int(x['score']) for x in user['achievements']])
+        cells[4].value = f'http://lomonosov-msu.ru/rus/user/profile/{user_id}'
+        worksheet.update_cells(cells)
